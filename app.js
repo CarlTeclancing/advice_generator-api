7f791ea917ed0c65e0e94f926527c40fa8a84fc3
YuvCarl <carldrake969@gmail.com>
2022-10-03 10:47:56 +0100

    updated the fetch equest api

---
+++ app.js
@@ -0,0 +1,35 @@
+
+  quategenerator =()=>{
+    fetch("https://api.adviceslip.com/advice")
+  .then(function(response) {
+    return response.json();
+    
+  })
+  .then(function(data) {
+    const { slip } = data;
+    //let add = JSON.stringify(data)
+    //let advice = Math.floor(Math.random() * data.length)
+    document.getElementById("advice").innerHTML = slip.advice;
+      console.table(slip.advice);
+
+    
+  });
+
+  }
+  quategenerator();
+/*
+  document.getElementById("btn").addEventListener("mousedown", fetchAdvice);
+const quote = document.querySelector(".card__quote");
+const quoteId = document.querySelector(".quote__id");
+
+async function fetchAdvice() {
+    const response = await fetch("https://api.adviceslip.com/advice", {
+        method: "GET",
+        cache: "reload",
+    });
+    const data = await response.json();
+    const { slip } = data;
+    quote.innerText = slip.advice;
+    quoteId.innerText = slip.id;
+}
+*/
